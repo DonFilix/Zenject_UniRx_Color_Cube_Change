db8af537e6f146613cc24299bfd35e95020ca575
DonFilix <jovan.cokleski@gmail.com>
2018-05-29 06:10:42 +0200

    test2
    test2

--- Assets/Scripts/GameSettingsInstaller.cs
+++ Assets/Scripts/GameSettingsInstaller.cs
@@ -6,6 +6,7 @@ using Zenject;
 [CreateAssetMenu(fileName = "GameSettingsInstaller", menuName = "Installers/GameSettingsInstaller")]
 public class GameSettingsInstaller : ScriptableObjectInstaller<GameSettingsInstaller>
 {
+
    public class ChangeColor : MonoBehaviour
    {
        public Material[] Materials;
@@ -17,12 +18,20 @@ public class GameSettingsInstaller : ScriptableObjectInstaller<GameSettingsInsta
        {
            Renderer.sharedMaterial = Materials [index];
        }
+       List<Color> colors= new List<Color>();
+       //colors.Add( new Color("blue"));
+       //colors.Add( new Color("yellow"));
+       //colors.Add( new Color("green"));
+
+       // Material mt = new Material(GetComponent<Renderer>().sharedMaterial);
+       //mt.color = IColor;
+       //GetComponent<Renderer>().material = mt;
       
     }
    
     public override void InstallBindings()
     {
-       // Container.Bind<ChangeColor>().To<colors1>().AsSingle();
+        //Container.Bind<ChangeColor>().To<colors1>().AsSingle();
         //Container.Bind<IColor>().To<colors2>().AsSingle();
         //Container.Bind<IColor>().To<colors3>().AsSingle();
         //Container.Bind<Material>().To<>(Material).AsSingle().WhenInjectedInto<Material>();
